534c7f6e6e4689c8de099cfbf8f1bbfab8042823
Aditya A <adityaaryan000@gmail.com>
2021-07-07 23:45:21 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+const adminRoutes = require('./routes/admin.js'); 
+const shopRoutes = require('./routes/shop.js'); 
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views', 'notfound.html'));
+})
+
+app.listen(3000,()=>{
+    console.log("Server is listening!");
+});
+
+
+
+
+
+
+
