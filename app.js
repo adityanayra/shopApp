e45de4fe86818f72aaaaab948b93fab76bfa821c
Aditya A <adityaaryan000@gmail.com>
2021-07-29 22:01:07 +0530

    Frontend changes to be made

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
 const app = express();
-const path = require('path');
-const adminRoutes = require('./routes/admin.js'); 
-const shopRoutes = require('./routes/shop.js'); 
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('5baa2528563f16379fc8a610')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', 'notfound.html'));
-})
+app.use(errorController.get404);
 
-app.listen(3000,()=>{
-    console.log("Server is listening!");
+mongoConnect(() => {
+  app.listen(3000);
 });
-
-
-
-
-
-
-
